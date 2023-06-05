2d104608b3711df59db20fa2fc017748fb3d48dc
AnatSid <tolyasha5100981@gmail.com>
2023-06-05 18:08:21 +0300

    delete variable userbook.

--- src/GetAllUsersCommand.java
+++ src/GetAllUsersCommand.java
@@ -1,9 +1,6 @@
 public class GetAllUsersCommand implements Command {
-    private UsersBook usersBook;
-
     @Override
     public void exucute(UsersBook usersBook) {
-        this.usersBook = usersBook;
         usersBook.findAll().forEach(System.out::println);
     }
 }
