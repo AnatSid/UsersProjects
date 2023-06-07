ada674c09309fad41560b3faf417f90553917478
AnatSid <tolyasha5100981@gmail.com>
2023-06-07 19:10:57 +0300

    wrapping a line: Commands commands = new Commands(usersBook) in a loop while

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
         UsersBook usersBook = new UsersBook();
-        Commands commands = new Commands(usersBook);
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
+            Commands commands = new Commands(usersBook);
             String inputCommand = scanner.nextLine();
             commands.executeCommand(inputCommand);
         }
