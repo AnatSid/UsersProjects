57e028fd67ffff280d1b46bd451025559c00dbc5
AnatSid <tolyasha5100981@gmail.com>
2023-06-26 21:01:45 +0300

    Init#6 update UserBookTest

--- src/test/java/UsersBookTest.java
+++ src/test/java/UsersBookTest.java
@@ -10,55 +10,60 @@ import static org.junit.jupiter.api.Assertions.*;
 class UsersBookTest {
 
     UsersBook usersBook;
-    User user;
+    User user1;
+    User user2;
+    User user3;
+
 
     @BeforeEach
     void setUp() {
         usersBook = new UsersBook();
-        user = new User("name1", "surname1", 20);
+        user1 = new User("name1", "surname1", 20);
+        user2 = new User("name2", "surname2", 22);
+        user3 = new User("name3", "surname3", 24);
     }
 
     @Test
     void addUser() {
-        usersBook.addUser(user);
-        assertEquals(user, usersBook.getUserById(user.getId()));
+        usersBook.addUser(user1);
+        assertEquals(user1, usersBook.getUserById(user1.getId()));
     }
 
     @Test
     void findAll() {
-        User user2 = new User("name2", "surname2", 22);
         List<User> userArrayList = new ArrayList<>();
-        userArrayList.add(user);
+        userArrayList.add(user1);
         userArrayList.add(user2);
+        userArrayList.add(user3);
 
-        usersBook.addUser(user);
+        usersBook.addUser(user1);
         usersBook.addUser(user2);
+        usersBook.addUser(user3);
 
         assertEquals(userArrayList, usersBook.findAll());
-
     }
 
     @Test
     void getUserById() {
-        usersBook.addUser(user);
-        int id = user.getId();
-        assertEquals(user, usersBook.getUserById(id));
+        usersBook.addUser(user1);
+        int id = user1.getId();
+        assertEquals(user1, usersBook.getUserById(id));
     }
 
     @Test
     void removeById() {
-        int id = user.getId();
-        usersBook.addUser(user);
+        int id = user1.getId();
+        usersBook.addUser(user1);
         usersBook.removeById(id);
         assertNull(usersBook.getUserById(id));
     }
 
     @Test
     void getLastAddedUser() {
-        User user2 = new User("name2", "surname2", 22);
-        User user3 = new User("name3", "surname3", 24);
-        usersBook.addUser(user);
+        usersBook.addUser(user1);
+        assertEquals(user1, usersBook.getLastAddedUser());
         usersBook.addUser(user2);
+        assertEquals(user2, usersBook.getLastAddedUser());
         usersBook.addUser(user3);
         assertEquals(user3, usersBook.getLastAddedUser());
     }
