db702f9a9fa9c6ab2dba93df0403e90382816dec
AnatSid <tolyasha5100981@gmail.com>
2023-07-18 17:36:16 +0300

    1) create test for RemoveUserByIdCommand class 2)Rename interface Book to UserBook

---
+++ src/test/java/RemoveUserByIdCommandTest.java
@@ -0,0 +1,50 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+class RemoveUserByIdCommandTest {
+
+    @Test
+    void ShouldPrintUserWithIdHasBeenDeletedIfUserIdPresentInUsersBook() {
+
+        User user = new User("name", "surname", 20);
+        FakeUserbook usersBook = new FakeUserbook(user);
+
+        FakeConsole console = new FakeConsole("1");
+        Command command = new RemoveUserByIdCommand(usersBook, console);
+
+        boolean isConsoleEmpty = console.messages.isEmpty();
+        Assertions.assertTrue(isConsoleEmpty);
+
+        command.execute();
+
+        Assertions.assertTrue(usersBook.userRemoved, "method remove is not called");
+
+        boolean isUserNotFoundMessagePresent = console.messages
+                .stream()
+                .anyMatch(message -> message.startsWith("User with id: 1 -> has been deleted"));
+
+        Assertions.assertTrue(isUserNotFoundMessagePresent, "Message is Empty");
+
+    }
+
+    @Test
+    void shouldPrintUserNotFoundWhenUserIdNotPresentInUsersBook() {
+
+        UserBook usersBook = new FakeUserbook(null);
+        FakeConsole console = new FakeConsole("2");
+
+        Command command = new RemoveUserByIdCommand(usersBook, console);
+
+        boolean isConsoleEmpty = console.messages.isEmpty();
+        Assertions.assertTrue(isConsoleEmpty);
+
+        command.execute();
+
+        boolean isUserNotFoundMessagePresent = console.messages
+                .stream()
+                .anyMatch(message -> message.startsWith("User with id: 2 not found"));
+
+        Assertions.assertTrue(isUserNotFoundMessagePresent, "Message is Empty. 'Else' block failed");
+    }
+}
