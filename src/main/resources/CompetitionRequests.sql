8b19cdbfdb72f3e1d9e8f07de859a02c5e73bf17
AnatSid <tolyasha5100981@gmail.com>
2023-11-21 22:21:55 +0300

    Adjusting queries

--- src/main/resources/CompetitionRequests.sql
+++ src/main/resources/CompetitionRequests.sql
@@ -22,32 +22,70 @@ JOIN
 ORDER BY result_time
 
 
---- Топ 3 спортсмена, которые набрали больше всего очков на данных соревнованиях
-SELECT ath.surname,ath.first_name, rr.points
-FROM RaceResult rr
-JOIN Athletes ath ON rr.id_athlete = ath.id
-ORDER BY points DESC
+---+Топ 3 спортсмена, которые набрали больше всего очков на конкретных соревнованиях
+SELECT
+    a.first_name,
+    a.surname,
+    a.date_birth,
+    SUM(rr.points) AS total_points
+FROM
+    RaceResult rr
+JOIN
+    Athletes a ON rr.id_athlete = a.id
+WHERE
+    rr.id_competition = 1
+GROUP BY
+    a.id
+ORDER BY
+    total_points DESC
 LIMIT 3;
 
-----Cписок участников на соревнованиях по дистанциям
-SELECT ath.surname,ath.first_name, r.distance || ' ' || r.style AS distance
+----Cписок участников на конкретных соревнованиях, и на конкретной дистанции
+SELECT ath.surname, ath.first_name, r.style,r.distance, comp.name AS Competition
 FROM RaceResult rr
 JOIN Athletes ath ON rr.id_athlete = ath.id
 JOIN Race r ON rr.id_race = r.id
+JOIN Competitions comp ON rr.id_competition = comp.id
+WHERE rr.id_competition = 1 AND rr.id_race = 4
+GROUP BY ath.id,r.style,r.distance,comp.name
 
 ------список результатов ( ФИО + дистанция + результат)
-SELECT ath.surname,ath.first_name, r.distance || ' ' || r.style AS distance,
-rr.result_time
-FROM RaceResult rr
-JOIN Athletes ath ON rr.id_athlete = ath.id
-JOIN Race r ON rr.id_race = r.id
-ORDER BY result_time
+SELECT
+ath.surname,
+ath.first_name,
+r.style,
+r.distance,
+rr.result_time,
+comp.name AS Competition
+FROM
+RaceResult rr
+JOIN
+Athletes ath ON rr.id_athlete = ath.id
+JOIN
+Race r ON rr.id_race = r.id
+JOIN
+Competitions comp ON rr.id_competition = comp.id
+WHERE
+rr.id_competition = 1 AND rr.id_race = 4
+GROUP BY
+ath.id,r.style,r.distance, rr.result_time,comp.name
+ORDER BY
+result_time
 
----------топ 3 города, которые набрали больше всего очков на данных соревнованиях
-SELECT c.name AS city, SUM(rr.points) AS total_points
-FROM RaceResult rr
-JOIN Athletes ath ON rr.id_athlete = ath.id
-JOIN City c ON ath.city_id = c.id
-GROUP BY city
-ORDER BY total_points DESC
-LIMIT 3
+---------+топ 3 города, которые набрали больше всего очков на конкретных соревнованиях
+SELECT
+    c.name AS city,
+    SUM(rr.points) AS total_points
+FROM
+    RaceResult rr
+JOIN
+    Athletes a ON a.id = rr.id_athlete
+JOIN
+    City c ON a.city_id = c.id
+WHERE
+    rr.id_competition = 1
+GROUP BY
+   c.id
+ORDER BY
+    total_points DESC
+LIMIT 3;
